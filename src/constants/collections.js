a4d037f94dae5c2199eb9c712630a2a0093f89f4
0x4227 <0x4227@gmail.com>
2024-10-31 21:54:57 +0000

    link

--- src/constants/collections.js
+++ src/constants/collections.js
@@ -4,5 +4,5 @@ export const GAME_COLLECTION = ENVIRONMENT+"games";
 export const NOTIFICATION_COLLECTION = ENVIRONMENT+"notification";
 export const USER_COLLECTION = ENVIRONMENT+"users";
 export const CONTRIBUTION_COLLECTION = ENVIRONMENT+"contribution";
-export const SHARE_LINK_URL = "https://arina-kappa.vercel.app/";
+export const SHARE_LINK_URL = "https://arina-private.vercel.app/";
 export const VOTE_COLLECTION = ENVIRONMENT+"winner_voting";
